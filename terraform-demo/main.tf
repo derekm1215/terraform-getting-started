38d9e4e2bec359ea44b3f717464c999f42754e24
derekm1215 <derekm1215@gmail.com>
2023-04-12 18:18:27 +0000

    Updated terraform-getting-started guide

---
+++ terraform-demo/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "training"
+  ports {
+    internal = 80
+    external = 80
+  }
+}
